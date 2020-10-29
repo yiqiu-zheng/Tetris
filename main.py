cdc3e7e3c503f7507a342ff0c7340db2ad71b852
yiqiu-zheng <73640680+yiqiu-zheng@users.noreply.github.com>
2020-10-29 20:58:51 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/27 14:40
+# @Author  : mirko
+# @FileName: main.py
+# @Software: PyCharm
+from PyQt5.QtWidgets import QDesktopWidget, QApplication
+from Panel.MainPanel import GamePanel
+import sys
+
+if __name__ == '__main__':
+        app = QApplication(sys.argv)
+        win = GamePanel()
+        win.show()
+        sys.exit(app.exec_())
