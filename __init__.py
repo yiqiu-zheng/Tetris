79a6a58bd26cfcace026fe35592d4eb146ef2d91
yiqiu-zheng <73640680+yiqiu-zheng@users.noreply.github.com>
2020-10-30 13:50:58 +1100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/24 10:10 PM
+# @Author  : mirko
+# @FileName: __init__.py.py
+# @Software: PyCharm
+
+
+
+GRID_PIXEL = 20
+
+
+#grid nums
+GRID_X_NUMS = 15
+GRID_Y_NUMS = 25
+
+
+CORE_PANEL_WIDTH = GRID_X_NUMS * GRID_PIXEL
+
+CORE_PANEL_HEIGHT = GRID_Y_NUMS * GRID_PIXEL
+
+
+SIDE_PANEL_WIDTH= 200
+
+FPS = 300
+
+
+COLORLIST = [0xCD7F32, 0xCC6666, 0x66CC66, 0x6666CC, 0xCCCC66, 0xCC66CC, 0x66CCCC, 0xDAAA00]
